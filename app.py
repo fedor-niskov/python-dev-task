d138211248e198ca59abcdaf07c7c8c407e488e7
Fedor Nis'kov <fedor.niskov@ispras.ru>
2019-03-12 23:37:53 +0300

    Added the counter button.
    This button shows the counter
    and increases it with every click.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from tkinter import *
 
+def inc_count():
+	global counter
+	counter += 1
+	b_count_text.set("Pressed: " + str(counter))
+
 root = Tk()
+
 b_exit = Button(root, command=root.quit, text="Exit")
 b_exit.grid()
+
+counter = 0
+b_count_text = StringVar()
+b_count_text.set("Pressed: 0")
+b_count = Button(root, command=inc_count, textvariable=b_count_text)
+b_count.grid()
+
 root.mainloop()
 
