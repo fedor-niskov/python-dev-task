d8eb146269a2f57ff7ab3a84b59b94ef5306cbeb
Fedor Nis'kov <fedor.niskov@ispras.ru>
2019-03-12 23:43:09 +0300

    Added the reset button.
    This button sets the counter to 0.

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ def inc_count():
 	counter += 1
 	b_count_text.set("Pressed: " + str(counter))
 
+def reset_count():
+	global counter
+	counter = 0
+	b_count_text.set("Pressed: 0")
+
 root = Tk()
 
 b_exit = Button(root, command=root.quit, text="Exit")
@@ -15,6 +20,9 @@ b_count_text = StringVar()
 b_count_text.set("Pressed: 0")
 b_count = Button(root, command=inc_count, textvariable=b_count_text)
 b_count.grid()
+
+b_reset = Button(root, command=reset_count, text="Reset")
+b_reset.grid()
 
 root.mainloop()
 
